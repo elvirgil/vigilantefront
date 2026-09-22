902ed01bb225979782c45dc6eb5eacef1ddc5208
elvirgil <virgiliocastillo@gmail.com>
2026-09-21 22:08:01 -0500

    Agregando rutas de configuración
    Agregando rutas de configuración

---
+++ app/pages/endpoints/(administration)/crear.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <p>Aquí se crea un nuevo endpoint</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
---
+++ app/pages/endpoints/(administration)/listar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <p>Aquí se listan los endpoints que estoy administrando</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
