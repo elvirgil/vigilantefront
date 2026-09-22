ba0a9b5d8e6d76c6181900ea5161c161b687e91a
virtual virgil <virtualvirgil@email.com>
2026-09-21 18:28:35 -0700

    Primer commit

---
+++ app/components/endpoints-list.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+    <div>
+<p>Hola este es mi componente</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
